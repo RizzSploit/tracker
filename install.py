21ce6ccb75ed5701f743e309921c3d6d210d229e
RizzSploit <65691315+RizzSploit@users.noreply.github.com>
2020-09-25 15:31:24 +0800

    INSTALL.PY

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+import time
+import sys, random
+
+os.system('apt install ruby -y && gem install lolcat')
+
+#accept
+print ("[!] Starting IP-Tracker...")
+time.sleep(3)
+os.system('clear')
+os.system('sh ip.sh')
